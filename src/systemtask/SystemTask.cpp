584c17a2b7bb25f4306354f73ef6a7687e96cc76
Victor Kareh <vkareh@redhat.com>
2024-02-06 15:41:59 -0500

    MusicService: Autostart music app on music start
    When starting music on a paired device, the music service will
    automatically trigger the music app to open if there is no app already
    loaded.

--- src/systemtask/SystemTask.cpp
+++ src/systemtask/SystemTask.cpp
@@ -385,6 +385,9 @@ void SystemTask::Work() {
             nimbleController.DisableRadio();
           }
           break;
+        case Messages::OnMusicStarted:
+          displayApp.PushMessage(Pinetime::Applications::Display::Messages::MusicStarted);
+          break;
         default:
           break;
       }
